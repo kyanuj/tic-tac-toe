2cd542d131f7a204b65b480381ec68f309e4c14a
Juan Carlos Pedroso <kyanuj@gmail.com>
2022-06-22 19:14:45 -0300

    Haciendo el componente Square iteractivo

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,19 +5,19 @@ function Board() {
         <div>
           <div className="status">'Next player: X'</div>
           <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square i='0' />
+            <Square i='1'/>
+            <Square i='2'/>
           </div>
           <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square i='3'/>
+            <Square i='4'/>
+            <Square i='5'/>
           </div>
           <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square i='6'/>
+            <Square i='7'/>
+            <Square i='8'/>
           </div>
         </div>
       );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,14 @@
-function Square () {
+import React, { useState } from 'react';
+
+function Square (props) {
+
+  const [value, setValue] = useState(null);
+
   return (
-    <button className="square">
-      {/* TODO */}
+    <button 
+      className="square"
+      onClick={() => setValue('X')}>
+      { value }
     </button>
   );
 }
